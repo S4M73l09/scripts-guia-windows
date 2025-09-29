a57716f159c6a37a6e158eaf05dca8d1975806c2
S4M73l09 <50885843+S4M73l09@users.noreply.github.com>
2025-09-29 14:19:52 +0200

    creaDominio.ps1

--- creaDominio.ps1
+++ creaDominio.ps1
@@ -1,7 +1,8 @@
-#Crea un dominio en windows core
-$dominioFQDN = "coresajesu.local"
-$dominioNETBIOS = "coresajesu"
-$passDominio = "p@ssw0rd.22"
+#Crea un dominio en windows core / Create Domain in windows Core 
+$dominioFQDN = "tunombre.local"
+$dominioNETBIOS = "tunombre"
+$passDominio = "p@ssword.22"
 Install-WindowsFeature AD-Domain-Services,DNS
 Import-module ADDSDeployment
+
 Install-ADDSForest -DomainName $dominioFQDN -DomainNetBiosName $dominioNETBIOS -SafeModeAdministratorPassword (ConvertTo-SecureString -string $passDominio -AsPlainText -Force) -DomainMode WinThreshold -ForestMode WinThreshold -InstallDNS -Confirm:$false
