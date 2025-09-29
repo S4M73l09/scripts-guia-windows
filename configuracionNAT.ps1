8d6dcdafd34bc0b821adf8000f26d421de578190
S4M73l09 <50885843+S4M73l09@users.noreply.github.com>
2025-09-29 14:17:44 +0200

    configuracionNAT.ps1

--- configuracionNAT.ps1
+++ configuracionNAT.ps1
@@ -1,5 +1,6 @@
-#Configuración de NAT
-#Instalamos la característica 
+#ConfiguraciÃ³n de NAT / Configuration Nat
+#Instalamos la caracterÃ­stica / Install the feature 
 Install-WindowsFeature Routing -IncludeManagementTools
-#Configuramos NAT para que la red interna tenga acceso al exterior
+#Configuramos NAT para que la red interna tenga acceso al exterior / we configure NAT so that the internal Network has access outside
+
 New-NetNat -Name "IntNetCore" -InternalIPInterfaceAddressPrefix "192.168.99.0/24"
