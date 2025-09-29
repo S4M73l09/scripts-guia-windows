8689400dba920c826aee1a2f103d5a2cd9ba0bb7
S4M73l09 <50885843+S4M73l09@users.noreply.github.com>
2025-09-29 14:13:12 +0200

    Configuracion DHCP y ambito.ps1

--- Configuracion DHCP y ambito.ps1
+++ Configuracion DHCP y ambito.ps1
@@ -1,16 +1,17 @@
 ﻿#Configuracion DHCP
-$nombreDC="coresajesu.local"
-$nombreAmbito="coresajesu"
-$ipDNS="192.168.99.1"
-$ipGW= "192.168.99.1"
-$ipAmbito= "192.168.99.0"
-$ipInicial= "192.168.99.20"
-$ipFinal= "192.168.99.40"
-$mascara= "255.255.255.0"
+$nombreDC="nombretuyo.local"
+$nombreAmbito="Nombretuyo"
+$ipDNS="192.168.x.x"
+$ipGW= "192.168.x.x"
+$ipAmbito= "192.168.x.x"
+$ipInicial= "192.168.x.x"
+$ipFinal= "192.168.x.x"
+$mascara= "255.255.255.255" # Cuidado con la mascara / Caution to net Mask
 #Instalar el rol de DHCP
 install-windowsFeature -name DHCP
 #Definir el ambito de DHCP
 add-Dhcpserverv4Scope -name $nombreAmbito -StartRange $ipInicial -endrange $ipFinal -subnetMask $mascara
 set-dhcpserverv4Optionvalue -ScopeID $ipAmbito -DNSserver $ipFinal -Dnsdomain $nombreAmbito -Router $ipGW
 #Autorizacion
+
 add-DhcpserverInDC -Dnsdomain $nombreDC
